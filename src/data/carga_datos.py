222335e603dfa603341558e36006923d4e143691
David Delgado <ddelgadoo@unal.edu.co>
2026-06-04 19:51:15 -0500

    Añadido pipe carga de datos y preprocesamiento

---
+++ src/data/carga_datos.py
@@ -0,0 +1,67 @@
+
+
+import os
+from pydataxm.pydatasimem import ReadSIMEM
+from datetime import datetime, timedelta
+import pandas as pd
+import logging
+from preprocessing.preprocessing import preprocessing
+
+
+def descargar_rango(fecha_inicio: str, fecha_fin: str) -> pd.DataFrame:
+    """
+    Descarga de la API de XM la base de datos d55202
+    "Demanda real energía Colombia" y la preprocesa por lotes.
+    """
+    start = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    end = datetime.strptime(fecha_fin, '%Y-%m-%d')
+
+    dfs = []
+    current = start
+
+    while current < end:
+        chunk_end = min(current + timedelta(days=29), end)
+
+        # Formateamos las fechas en texto para la API
+        str_inicio = current.strftime('%Y-%m-%d')
+        str_fin = chunk_end.strftime('%Y-%m-%d')
+
+        print(f"Descargando periodo: {str_inicio} a {str_fin}...")
+
+        # 1. Descargamos el lote crudo
+        df_raw = ReadSIMEM("d55202", str_inicio, str_fin).main(filter=False)
+
+        # 2. Verificamos que la API realmente devolvió datos para no romper el código
+        if df_raw is not None and not df_raw.empty:
+
+            print(f" Preprocesando lote de {len(df_raw)} filas")
+
+            # 3. Aplicar pipeline de preprocesamiento al lote
+            df_clean = preprocessing(df_raw)
+
+            # 4. Guardamos el lote ya limpio
+            dfs.append(df_clean)
+        else:
+            print(f" No se encontraron datos para {str_inicio} - {str_fin}")
+
+        # Avanzar al siguiente periodo
+        current = chunk_end + timedelta(days=1)
+
+    print("Concatenando base de datos final")
+
+    # 5. Concatenamos los lotes que ya están preprocesados
+    if dfs:
+        df_final = pd.concat(dfs, ignore_index=True)
+        return df_final
+    else:
+        print("No se descargó ningún dato en el rango especificado.")
+        return pd.DataFrame()
+
+
+if __name__ == '__main__':
+
+    print(descargar_rango("2024-01-01", "2024-03-30").head())
+
+
+
+
---
+++ src/preprocessing/preprocessing.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import re
+import sys
+
+
+def verificacionFormatoFecha(df:pd.DataFrame):
+    patron_fecha = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$'
+    fechas = df['FechaHora']
+    cumple_formato = fechas.str.match(patron_fecha)
+
+    if cumple_formato.all():
+        print('Todas las fechas cumplen el formato YYYY-MM-DDTHH:MM:SS')
+    else:
+        raise ValueError("Se encontraron fechas que no cumplen el formato YYYY-MM-DDTHH:MM:SS\n",fechas[~cumple_formato])
+
+
+def reformatoFecha(df:pd.DataFrame) -> pd.DataFrame:
+
+    df['FechaHora'] = pd.to_datetime(df['FechaHora'])
+    print("reformato exitoso\n")
+    print("------------------")
+    return df
+
+def borrarCerosValores(df:pd.DataFrame) -> pd.DataFrame:
+
+    index_ceros = df[df['Valor'] == 0].index
+    df = df.drop(index_ceros)
+    print("Borrado de ceros exitoso\n")
+    print("------------------")
+    return df
+
+def borrar_columnas(df :pd.DataFrame, cols: list ) -> pd.DataFrame:
+
+    df = df.drop(cols, axis=1)
+    print("Borrado de columnas exitoso\n")
+    return df
+
+
+def ordenar_versiones_por_dato(df: pd.DataFrame) -> pd.DataFrame:
+
+    orden_versiones = {'TXF': 9, 'TXR': 8, 'TX8': 7, 'TX7': 6,
+                       'TX6': 5, 'TX5': 4, 'TX4': 3, 'TX3': 2, 'TX2': 1}
+
+    df['version_orden'] = (df['Version'].map(orden_versiones))
+    df = (df.sort_values('version_orden', ascending=False).
+          drop_duplicates(subset=['FechaHora', 'CodigoSICAgente'], keep='first'))
+    print("Ordenado exitoso\n")
+    return df
+
+def groupbyPorHora(df: pd.DataFrame):
+
+    df = df.groupby('FechaHora', as_index=False)['Valor'].sum()
+    print("Groupby por hora exitoso\n")
+    return df
+
+def horasCíclicas(df:pd.DataFrame) -> pd.DataFrame:
+
+    df['hora_sin'] = np.sin(
+        2 * np.pi * df['FechaHora'].dt.hour / 24)
+
+    df['hora_cos'] = np.cos(
+        2 * np.pi * df['FechaHora'].dt.hour / 24)
+
+    df['dia_semana_sin'] = np.sin(
+        2 * np.pi * df['FechaHora'].dt.dayofweek / 7)
+
+    df['dia_semana_cos'] = np.cos(
+        2 * np.pi * df['FechaHora'].dt.dayofweek / 7)
+
+    df['mes_sin'] = np.sin(
+        2 * np.pi * df['FechaHora'].dt.month / 12)
+
+    df['mes_cos'] = np.cos(
+        2 * np.pi * df['FechaHora'].dt.month / 12)
+
+    print("Horas exitoso\n")
+    return df
+
+
+def preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+    verificacionFormatoFecha(df)
+
+    df = df.copy()
+    df_preprocesado = (df
+                       .pipe(reformatoFecha)
+                       .pipe(borrarCerosValores)
+                       .pipe(borrar_columnas,cols = ['CodigoVariable','CodigoDuracion', 'UnidadMedida'])
+                       .pipe(groupbyPorHora)
+                       .pipe(horasCíclicas)
+                       )
+    df_preprocesado = df_preprocesado.drop(columns =['FechaHora'])
+    return df_preprocesado
+
